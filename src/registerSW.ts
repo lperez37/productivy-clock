91e8ca1e11ff825b772408b67c4fbdaef14d1a10
lperez37 <luis@caselles.nl>
2024-12-10 02:42:54 +0100

    build error fix

--- src/registerSW.ts
+++ src/registerSW.ts
@@ -2,6 +2,9 @@ export function registerSW() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('/sw.js')
+      .then(() => {
+        console.log('ServiceWorker registration successful')
+      })
       .catch((error) => {
         console.error('ServiceWorker registration failed:', error)
       })
